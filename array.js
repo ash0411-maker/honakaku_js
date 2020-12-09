2efb25db02cc53553cce12c80877e25c9fc70471
Uetani <satoshi.0411@icloud.com>
2020-12-09 11:21:11 +0900

    chap5

---
+++ array.js
@@ -0,0 +1,59 @@
+// function getMaxMin(...nums) {
+// 	return [Math.min(...nums), Math.max(...nums) ]
+// }
+
+// let result = getMaxMin(10, 35, -5, 78, 0);
+// console.log(result); //結果：[78, -5]
+
+
+// let [max, min] = getMaxMin(10, 35, -5, 78, 0);
+// // 先に実行される関数から値が順番に格納されていく？？？？
+
+// console.log(max);
+// console.log(min);
+
+
+
+// ===============================================
+
+
+// function arrayWalk(data, f) {
+//   for (var key in data) {
+//     f(data[key], key);
+//   }
+// }
+
+// function showElement(value, key) {
+//   console.log(key + '：' + value);
+// }
+
+// var ary = [1, 2, 4, 8, 16];
+// arrayWalk(ary, showElement);
+
+
+
+// function arrayWalk(data, f) {
+//   for (var key in data) {
+//     f(data[key], key);
+//   }
+// }
+
+// var ary = [1, 2, 4, 8, 16];
+// arrayWalk(ary, function (value, key) {
+//     console.log(key + '：' + value);
+//   }
+// );
+
+
+// function closure(init) {
+//   let counter = init;
+
+//   return function() {
+//     return ++counter;
+//   }
+// }
+
+// let myClosure = closure(1);
+// console.log(myClosure());
+// console.log(myClosure());
+// console.log(myClosure());
--- samples/js/chap02/scripts/array.js
+++ samples/js/chap02/scripts/array.js
@@ -1,2 +1,10 @@
 var data = ['JavaScript', 'Ajax', 'ASP.NET'];
 console.log(data[0]);
+
+let book = {
+  title: "カンファレンス",
+  publish: "講談社",
+  price: 2490
+};
+
+let {price, title, memo = "なし"} = book;
