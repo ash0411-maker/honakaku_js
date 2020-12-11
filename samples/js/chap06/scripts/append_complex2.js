a499bed592b289cff7dd66b52290b78caddb24fa
Uetani <satoshi.0411@icloud.com>
2020-12-11 15:20:43 +0900

    DocumentFragmentオブジェクト

--- samples/js/chap06/scripts/append_complex2.js
+++ samples/js/chap06/scripts/append_complex2.js
@@ -1,19 +1,22 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener("DOMContentLoaded", function() {
   var books = [
     { title: '独習PHP 第3版', price: 3200 },
     { title: 'Javaポケットリファレンス', price: 2680 },
     { title: 'アプリを作ろう！Android入門', price: 2000 }
   ];
 
-  var frag = document.createDocumentFragment();
+  // let list = document.getElementById("list");
+  let frag = document.createDocumentFragment();
 
-  for(var i = 0, len = books.length; i < len; i++) {
-    var b = books[i]
-    var li = document.createElement('li');
-    var text = document.createTextNode(b.title + '：'+ b.price + '円');
+  for(let i = 0; i < books.length; i++) {
+    let book = books[i]
+    let li = document.createElement("li");
+    let text = document.createTextNode(book.title + "：" + book.price + "円");
     li.appendChild(text);
+    // list.appendChild(li);
     frag.appendChild(li);
   }
 
+  //li要素をまとめて文章ツリーに追加
   document.getElementById('list').appendChild(frag);
-}, false);
+}, false)
