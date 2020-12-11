2c4341ca90d5f94c1cc6ffcc1163735f5d7ce10b
Uetani <satoshi.0411@icloud.com>
2020-12-11 16:51:24 +0900

    キーイベント

--- samples/js/chap06/scripts/event.js
+++ samples/js/chap06/scripts/event.js
@@ -3,7 +3,7 @@ document.addEventListener("DOMContentLoaded", function() {
   // 引数は慣例的にeventを表す「e」や「ev」とするのが一般的
   document.getElementById("btn").addEventListener("click", function(e) {
     let target = e.target;
-    console.log("発生元： " + target.nodeName + "/" + target.id);
+    console.log("発生元： " + target.nodeName + "/" + target.id + "/" + target);
     console.log("種類： " + e.type);
   }, false)
 }, false)
--- samples/js/chap06/scripts/event_key.js
+++ samples/js/chap06/scripts/event_key.js
@@ -1,5 +1,12 @@
-document.addEventListener('DOMContentLoaded', function() {
-  document.getElementById('key').addEventListener('keydown', function(e) {
-    console.log('キーコード：' + e.keyCode);
-  }, false);
-}, false);
+// document.addEventListener('DOMContentLoaded', function() {
+//   document.getElementById('key').addEventListener('keydown', function(e) {
+//     console.log('キーコード：' + e.keyCode);
+//   }, false);
+// }, false);
+
+
+document.addEventListener("DOMContentLoaded", function() {
+  document.getElementById("key").addEventListener("keydown", function(e) {
+    console.log("キーワード：" + e.keyCode)
+  }, false)
+}, false)
