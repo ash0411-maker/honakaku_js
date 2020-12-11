5840082651e380e23b4f66f171801f2dfee2ef38
Uetani <satoshi.0411@icloud.com>
2020-12-11 17:43:34 +0900

    arrow this

--- samples/js/chap06/scripts/this_arrow.js
+++ samples/js/chap06/scripts/this_arrow.js
@@ -1,21 +1,41 @@
-document.addEventListener('DOMContentLoaded', function() {
-  var Counter = function(elem) {
+// document.addEventListener('DOMContentLoaded', function() {
+//   var Counter = function(elem) {
+//     this.count = 0;
+//     this.elem = elem;
+//   //  elem.addEventListener('click', function() {
+//   //    this.count++;
+//   //    this.show();
+//   //  }, false);
+
+//       elem.addEventListener('click', () => {
+//       this.count++;
+//       this.show();
+//       }, false);
+//   };
+
+//   Counter.prototype.show = function() {
+//     console.log(this.elem.id + 'は' + this.count + '回クリックされました。');
+//   }
+
+//   var c = new Counter(document.getElementById('btn'));
+// }, false);
+
+
+
+document.addEventListener("DOMContentLoaded", function() {
+  let Counter = function(elem) {
     this.count = 0;
     this.elem = elem;
-  //  elem.addEventListener('click', function() {
-  //    this.count++;
-  //    this.show();
-  //  }, false);
 
-      elem.addEventListener('click', () => {
+    elem.addEventListener("click", () => {
       this.count++;
       this.show();
-      }, false);
+    }, false);
   };
 
   Counter.prototype.show = function() {
-    console.log(this.elem.id + 'は' + this.count + '回クリックされました。');
+    console.log(this.elem + "は" + this.count + "回クリックされました");
   }
 
-  var c = new Counter(document.getElementById('btn'));
-}, false);
+  let c = new Counter(document.getElementById('btn'));
+}, false)
