0cfe83530e9800d3a617ea80e9ac9b6d97763954
Uetani <satoshi.0411@icloud.com>
2020-12-11 16:33:34 +0900

    Eventオブジェクト

--- samples/js/chap06/scripts/event.js
+++ samples/js/chap06/scripts/event.js
@@ -1,7 +1,9 @@
-document.addEventListener('DOMContentLoaded', function() {
-  document.getElementById('btn').addEventListener('click', function(e) {
-    var target = e.target;
-    console.log('発生元：' + target.nodeName + '/' + target.id);
-    console.log('種類：' + e.type);
-  }, false);
-}, false);
+document.addEventListener("DOMContentLoaded", function() {
+  // イベントオブジェクトを受け取るために、イベントリスナーに引数を渡すだけ
+  // 引数は慣例的にeventを表す「e」や「ev」とするのが一般的
+  document.getElementById("btn").addEventListener("click", function(e) {
+    let target = e.target;
+    console.log("発生元： " + target.nodeName + "/" + target.id);
+    console.log("種類： " + e.type);
+  }, false)
+}, false)
