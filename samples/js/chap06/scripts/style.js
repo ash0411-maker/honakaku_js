c0d5d2ea0e2cc2cf47b70c29a415df8c7bca1ee0
Uetani <satoshi.0411@icloud.com>
2020-12-11 15:47:12 +0900

    styleプロパティ

--- samples/js/chap06/scripts/style.js
+++ samples/js/chap06/scripts/style.js
@@ -1,11 +1,13 @@
-document.addEventListener('DOMContentLoaded', function() {
-  var elem = document.getElementById('elem');
+document.addEventListener("DOMContentLoaded", function() {
+  let elem = document.getElementById("elem");
 
-  elem.addEventListener('mouseover', function() {
-    this.style.backgroundColor = 'Yellow';
-  }, false);
+  elem.addEventListener("mouseover", function() {
+    this.style.backgroundColor = "red";
+    this.style.color = "white"
+  }, false)
 
-  elem.addEventListener('mouseout', function() {
-    this.style.backgroundColor = '';
-  }, false);
-}, false);
+  elem.addEventListener("mouseout", function() {
+    this.style.backgroundColor = "";
+    this.style.color = "black"
+  })
+})
