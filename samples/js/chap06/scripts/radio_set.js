a84b606a2b2289e5b1ac416411a2c09b26b5b9f9
Uetani <satoshi.0411@icloud.com>
2020-12-11 10:56:10 +0900

    setValue radiobutton

--- samples/js/chap06/scripts/radio_set.js
+++ samples/js/chap06/scripts/radio_set.js
@@ -1,15 +1,32 @@
-document.addEventListener('DOMContentLoaded', function() {
-  var setRadioValue = function(name, value) {
-    var elems = document.getElementsByName(name);
+// document.addEventListener('DOMContentLoaded', function() {
+//   var setRadioValue = function(name, value) {
+//     var elems = document.getElementsByName(name);
 
-    for(var i = 0, len = elems.length; i < len; i++) {
-      var elem = elems.item(i);
-      if (elem.value === value) {
+//     for(var i = 0, len = elems.length; i < len; i++) {
+//       var elem = elems.item(i);
+//       if (elem.value === value) {
+//         elem.checked = true;
+//         break;
+//       }
+//     }
+//   };
+
+//   setRadioValue('food', '焼き肉');
+// }, false);
+
+
+document.addEventListener("DOMContentLoaded", function() {
+  let setRadioValue = function(name, value) {
+    let elems = document.getElementsByName(name);
+
+    for(let i = 0, len = elems.length; i < len; i++) {
+      let elem = elems.item(i);
+      if(elem.value === value) {
         elem.checked = true;
         break;
       }
     }
-  };
-  
-  setRadioValue('food', '餃子');
+  }
+
+  setRadioValue("food", "ラーメン")
 }, false);
