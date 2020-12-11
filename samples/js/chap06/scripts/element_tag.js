dde1f58d4a800ab13cc4eb29bf49923bc863ae7a
Uetani <satoshi.0411@icloud.com>
2020-12-11 10:40:33 +0900

    radiobutton

--- samples/js/chap06/scripts/element_tag.js
+++ samples/js/chap06/scripts/element_tag.js
@@ -1,4 +1,9 @@
-var list = document.getElementsByTagName('a');
-for (var i = 0, len = list.length; i < len; i++) {
-  console.log(list.item(i).href);
+// var list = document.getElementsByTagName('a');
+// for (var i = 0, len = list.length; i < len; i++) {
+//   console.log(list.item(i).href);
+// }
+
+var list = document.getElementsByTagName("a");
+for (let i = 0, len = list.length; i < len; i++) {
+  console.log(list.item(i).href)
 }
--- samples/js/chap06/scripts/radio.js
+++ samples/js/chap06/scripts/radio.js
@@ -1,19 +1,40 @@
-document.addEventListener('DOMContentLoaded', function() {
-  var getRadioValue = function(name) {
-    var result = '';
-    var elems = document.getElementsByName(name);
+// document.addEventListener('DOMContentLoaded', function() {
+//   var getRadioValue = function(name) {
+//     var result = '';
+//     var elems = document.getElementsByName(name);
 
-    for(var i = 0, len = elems.length; i < len; i++) {
-      var elem = elems.item(i);
-      if (elem.checked) {
+//     for(var i = 0, len = elems.length; i < len; i++) {
+//       var elem = elems.item(i);
+//       if (elem.checked) {
+//         result = elem.value;
+//         break;
+//       }
+//     }
+//     return result;
+//   };
+
+//   document.getElementById('btn').addEventListener('click', function() {
+//     window.alert(getRadioValue('food'));
+//   }, false);
+// }, false);
+
+
+document.addEventListener("DOMContentLoaded", function() {
+  let getRadioValue = function(name) {
+    let result = "";
+    let elems = document.getElementsByName(name)
+
+    for(let i = 0, len = elems.length; i < len; i++) {
+      let elem = elems.item(i)
+      if(elem.checked) {
         result = elem.value;
         break;
       }
     }
     return result;
-  };
-  
-  document.getElementById('btn').addEventListener('click', function() {
-    window.alert(getRadioValue('food'));
-  }, false);
-}, false);
+  }
+
+  document.getElementById("btn").addEventListener("click", function() {
+    window.alert(getRadioValue("food"));
+  }, false)
+}, false)
