9e2a66ffaeca7beb1648829c33002d6cc679e375
Uetani <satoshi.0411@icloud.com>
2020-12-11 11:26:33 +0900

    option　デフォルトの要素選択　送信ボタンでalert表示

--- samples/js/chap06/scripts/list_set.js
+++ samples/js/chap06/scripts/list_set.js
@@ -9,6 +9,27 @@ document.addEventListener('DOMContentLoaded', function() {
       }
     }
   };
-  
+
+  // 選択されている要素を取得
+  var getSelectValue = function(name) {
+    var result = [];
+    var opts = document.getElementById(name).options;
+
+    for(var i = 0, len = opts.length; i < len; i++) {
+      var opt = opts.item(i);
+      if (opt.selected) {
+        result.push(opt.value);
+      }
+    }
+    return result;
+  };
+
+  // 送信を押したらalertに出力
+  document.getElementById('btn').addEventListener('click', function() {
+    window.alert(getSelectValue('food'));
+  }, false);
+
+
+  // デフォルトの要素選択
   setListValue('food', ['餃子', '焼き肉']);
 }, false);
